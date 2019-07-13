2a971ebf4e0bb81d97bd4858769b1d56f01829c3
GitKurmalex <alexandrmail32@gmail.com>
2019-07-13 20:21:14 +0300

    Updates

---
+++ precache-manifest.bf4b655459bc33c5844f9bfda6fcb3bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76dfffddd4f19d3000f1f756d1b423f9",
+    "url": "/react_redux-list-of-posts/index.html"
+  },
+  {
+    "revision": "8fc66cabe6469103507f",
+    "url": "/react_redux-list-of-posts/static/css/main.3d1849f5.chunk.css"
+  },
+  {
+    "revision": "b463b5d765fb3da07603",
+    "url": "/react_redux-list-of-posts/static/js/2.dccf7134.chunk.js"
+  },
+  {
+    "revision": "8fc66cabe6469103507f",
+    "url": "/react_redux-list-of-posts/static/js/main.5703ddc8.chunk.js"
+  },
+  {
+    "revision": "c3092fe5bcf6cd34b5b5",
+    "url": "/react_redux-list-of-posts/static/js/runtime~main.d0e8944a.js"
+  }
+]);
